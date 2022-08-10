e20d29a44ff71f2f4af628c715519e4968c4bd55
JustinGebertDesk <justin.gebert.berlin@gmail.com>
2022-08-10 17:52:51 +0200

    änderung testing 01

--- Testing01.java
+++ Testing01.java
@@ -1,6 +1,6 @@
 public class Testing01{
 
-    private String x = "hello Meister";
+    private String x = "hello Meister du hgundd";
 
     public static void main(String[] args) {
         System.out.println(x);
